d98222f8e9f1d061482f3ff65a233f4881c47a57
crispantufla <crispantufla@gmail.com>
2021-08-07 03:41:49 -0700

    await?

--- src/mongo/connection.js
+++ src/mongo/connection.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect('mongodb+srv://admin:1234@cluster0.9lknw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', { useUnifiedTopology: true ,useNewUrlParser: true, useCreateIndex: true })
-.then(db => console.log('db connected')).catch( err => console.log(err));
+const connect = async () => {
+    await mongoose.connect('mongodb+srv://admin:1234@cluster0.9lknw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', { useUnifiedTopology: true ,useNewUrlParser: true, useCreateIndex: true })
+}
+
+connect();
