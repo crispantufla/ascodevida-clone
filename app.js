59813bb1ca5c0ac8f5f63d0caf0f40f92349457b
crispantufla <crispantufla@gmail.com>
2021-08-07 03:16:22 -0700

    try

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const globalRouter = require('./src/controllers/globalRouter').globalRouter;
 
 const app = express();
-const port = 3001;
+const PORT = process.env.PORT || 3000;
 app.use(cors({
 	origin: '*',
 	optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
@@ -24,6 +24,6 @@ app.use((req, res, next) => {
     res.status(404).render("404");
 })
 
-app.listen(port, () => {
-	console.log(`App listening at http://localhost:${port}`)
+app.listen(PORT, () => {
+	console.log(`App listening at http://localhost:${PORT}`)
 })
