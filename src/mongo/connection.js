a1fc1a3f51b7686b56bb50ba398a0f0fc4ff5bc9
crispantufla <crispantufla@gmail.com>
2021-08-07 03:03:04 -0700

    catch connect

--- src/mongo/connection.js
+++ src/mongo/connection.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect('mongodb+srv://admin:1234@cluster0.9lknw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', { useUnifiedTopology: true ,useNewUrlParser: true, useCreateIndex: true });
+mongoose.connect('mongodb+srv://admin:1234@cluster0.9lknw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', { useUnifiedTopology: true ,useNewUrlParser: true, useCreateIndex: true })
+.then(db => console.log('db connected')).catch( err => console.log(err));
